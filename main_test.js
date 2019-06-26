132ff7157453dd15c766128d79a4c51dbd6b7105
aditya-deshpadne <40906607+aditya-deshpadne@users.noreply.github.com>
2019-06-26 07:29:21 +0530

    test UI files
    test UI files

--- main_test.js
+++ main_test.js
@@ -8,128 +8,91 @@ import {
   EuiPageContentHeader,
   EuiPageContentBody,
   EuiText,
-  EuiFieldText,
-  EuiForm,
-  EuiFormRow,
-  EuiButton,
-  EuiBasicTable,
+  EuiSuperSelect
 } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
 
 export class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-		
-		searchTerm :'',
-		resp : '',
-		 selectedItems: [],
+		options : [],
 		
 	};
-		
-	};
- onSelectionChange = (selectedItems) => {
-    this.setState({ selectedItems });
-		alert(selectedItems.length)
-	   alert(selectedItems[0].id)
-  };
-
-  onSearchTextChange(event) {
-	  
-	  this.state.searchTerm = event.target.value;
   }
+
+  
   
   searchData(){
 	const { httpClient } = this.props;
-    httpClient.post('../api/HelloWorld/update_test',{"searchterm" : this.state.searchTerm
+    httpClient.post('../api/Hello6.7/search',{"searchterm" : "temp"
 	}).then((response) => {
-      this.setState({ resp: response.data });
-	  window.alert(this.state.resp);
+      alert(response)
+	  let jsonData = JSON.parse(response.data.result)
+	  alert(jsonData)
+	  this.setState({ options :jsonData})
 	});
-  }
-   
-
+  }	
+  
+  
   componentDidMount() {
     /*
        FOR EXAMPLE PURPOSES ONLY.  There are much better ways to
        manage state and update your UI than this.
     */
     const { httpClient } = this.props;
-    httpClient.get('../api/HelloWorld/example').then((resp) => {
+    httpClient.get('../api/Hello6.7/example').then((resp) => {
       this.setState({ time: resp.data.time });
     });
-  } 
-   
-   
+	
+	this. searchData()
+	
+  }
   render() {
     const { title } = this.props;
-	
-	
-	const selection = {
-      onSelectionChange: this.onSelectionChange
-    };
-
-    const pageOfItems = [
-      {
-        id: "1",
-        name: "john"
-      },
-      {
-        id: "12",
-        name: "Don"
-      }
-    ];
-    const columns = [
-      {
-        field: "id",
-        name: "id",
-        truncateText: true
-      },
-      {
-        field: "name",
-        name: " Name",
-        truncateText: true
-      }
-    ];
-	
-	
     return (
       <EuiPage>
         <EuiPageBody>
           <EuiPageHeader>
             <EuiTitle size="l">
-              <h1> Hello World!</h1>
+              <h1>
+                <FormattedMessage
+                  id="hello67.helloWorldText"
+                  defaultMessage="{title} Hello World!"
+                  values={{ title }}
+                />
+              </h1>
             </EuiTitle>
           </EuiPageHeader>
           <EuiPageContent>
             <EuiPageContentHeader>
               <EuiTitle>
-                <h2>Congratulations</h2>
+                <h2>
+                  <FormattedMessage
+                    id="hello67.congratulationsTitle"
+                    defaultMessage="Congratulations"
+                  />
+                </h2>
               </EuiTitle>
             </EuiPageContentHeader>
             <EuiPageContentBody>
               <EuiText>
-                <h3>Custom Kibana Plugin!</h3>
-                <p>The server time (via API call) is {this.state.time || 'NO API CALL YET'}</p>
+                <h3>
+                  <FormattedMessage
+                    id="hello67.congratulationsText"
+                    defaultMessage="You have successfully created your first Kibana Plugin!"
+                  />
+                </h3>
+                <p>
+                  <FormattedMessage
+                    id="hello67.serverTimeText"
+                    defaultMessage="The server time (via API call) is {time}"
+                    values={{ time: this.state.time || 'NO API CALL YET' }}
+                  />
+                </p>
               </EuiText>
-			 <EuiForm >
-				<EuiFormRow
-					label="Search Field"
-					helpText="Please enter search value"
-				>
-					<EuiFieldText name="search_term" onChange={(event) => this.onSearchTextChange(event)}/>
-				</EuiFormRow>	
-				
-				<EuiButton  fill onClick={() => this.searchData()}>
-					Search
-				</EuiButton>
-				
-			</EuiForm>	
-			<EuiBasicTable
-			  items={pageOfItems}
-			  itemId="id"
-			  columns={columns}
-			  isSelectable={true}
-			  selection={selection}
+			   <EuiSuperSelect
+				options={this.state.options}
 			/>
             </EuiPageContentBody>
           </EuiPageContent>
